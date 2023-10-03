e5bf64d44166835a7d1e9c4041d2c2d0b0f214c3
MaximeCauchon <m.cauchon1985@gmail.com>
2023-10-03 14:24:10 +0200

    formulaire ajout sortie

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,19 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Etat;
 use App\Entity\Sortie;
+use App\Form\NouvelleSortieType;
+use App\Repository\EtatRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SortieController extends AbstractController
 {
     #[Route('/sorties', name: 'sorties_list')]
     public function index(): Response
     {
-        return $this->render('sortie/index.html.twig', [
+        return $this->render('sortie/affichage-sorties.html.twig', [
             'controller_name' => 'SortieController',
         ]);
     }
@@ -25,12 +28,16 @@ class SortieController extends AbstractController
         $sortie = new Sortie();
         $currentUser = $this->getUser();
         $sortie->setOrganisateur($currentUser);
-        $sortieForm = $this->createForm(NouvelleSortieFormType::class, $sortie);
-        $sortieForm->handleRequest($request);
+        $nouvelleSortieForm = $this->createForm(NouvelleSortieType::class, $sortie);
+        $nouvelleSortieForm->handleRequest($request);
+        $etatRepo = $entityManager->getRepository(Etat::class);
+        $sortie->setEtat($etatRepo->find(1));
 
-        if ($sortieForm->isSubmitted() && $sortieForm->isValid()) {
-            $sortie->
+        if ($nouvelleSortieForm->isSubmitted() && $nouvelleSortieForm->isValid()) {
 
+            if ($nouvelleSortieForm->get('publier')->isClicked()) {
+                $sortie->setEtat($etatRepo->find(2));
+            }
 
             $entityManager->persist($sortie);
             $entityManager->flush();
@@ -41,6 +48,7 @@ class SortieController extends AbstractController
 
         return $this->render('sortie/nouvelle-sortie.html.twig', [
             'controller_name' => 'SortieController',
+            'nouvelleSortieForm' => $nouvelleSortieForm->createView()
         ]);
     }
 }
--- src/Form/NouvelleSortieType.php
+++ src/Form/NouvelleSortieType.php
@@ -2,28 +2,68 @@
 
 namespace App\Form;
 
-use App\Entity\Sortie;
+use DateTime;
+use App\Entity\Lieu;
+use App\Entity\Ville;
+use App\Entity\Sortie;;
+
 use Symfony\Component\Form\AbstractType;
+use function Symfony\Component\Clock\now;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateIntervalType;
 
 class NouvelleSortieType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+
         $builder
-            ->add('nom')
-            ->add('dateHeureDebut')
-            ->add('duree')
-            ->add('dateLimiteInscription')
-            ->add('nbInscriptionMax')
-            ->add('infosSortie')
-            ->add('motifAnnulation')
-            ->add('participants')
-            ->add('organisateur')
-            ->add('etat')
-            ->add('lieu')
-        ;
+            ->add('nom', TextType::class, [
+                'label' => 'Nom de la sortie :'
+            ])
+            ->add('dateHeureDebut', DateTimeType::class, [
+                'label' => 'Date et heure de la sortie :'
+            ])
+            ->add('dateLimiteInscription', DateTimeType::class, [
+                'label' => "Date limite d'inscription :"
+            ])
+            ->add('nbInscriptionMax', IntegerType::class, [
+                'label' => 'Nombre de places :'
+            ])
+            ->add('duree', DateIntervalType::class, [
+                'label' => 'Durée :',
+                'with_years'  => false,
+                'with_months' => false,
+                'with_days'   => false,
+                'with_minutes'  => true,
+                'labels' => [
+                    'minutes' => "minutes",
+                ]
+            ])
+            ->add('infosSortie', TextType::class, [
+                'label' => 'Descriptions et infos :'
+            ])
+            ->add('ville', EntityType::class, [
+                'label' => 'Ville :',
+                'class' => Ville::class,
+                'choice_label' => 'nom',
+                'mapped' => false
+            ])
+            ->add('lieu', EntityType::class, [
+                'label' => 'Lieu :',
+                'class' => Lieu::class,
+                'choice_label' => 'nom'
+            ])
+            ->add('enregistrer', SubmitType::class, ['label' => 'Enregistrer'])
+            ->add('publier', SubmitType::class, ['label' => 'Publier'])
+            ->getForm();
     }
 
     public function configureOptions(OptionsResolver $resolver): void
