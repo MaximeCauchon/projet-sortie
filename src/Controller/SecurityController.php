68a83c42e174afb485d4153fc96f98a48d344d64
noemie <noemie@velorizons.com>
2023-10-03 12:00:24 +0200

    Page édition profil utilisateur + édition mot de passe utilisateur

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,8 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
+use App\Form\EditUserLoggedPasswordType;
+use App\Form\EditUserLoggedType;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -33,5 +40,44 @@ class SecurityController extends AbstractController
 	public function logout(): void
 	{
 		throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+	}
+
+	#[Route(path: '/edition_profil', name: 'security_editLoggedUser')]
+	public function editLoggedUser(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $userPasswordHasher): Response
+	{
+		$user = $this->getUser();
+		$formEditLoggedUser = $this->createForm(EditUserLoggedType::class, $user);
+		$formEditLoggedUser->handleRequest($request);
+
+		$formEditLoggedUserPassword = $this->createForm(EditUserLoggedPasswordType::class, $user);
+		$formEditLoggedUserPassword->handleRequest($request);
+
+		if ($formEditLoggedUser->isSubmitted() && $formEditLoggedUser->isValid()) {
+
+			$entityManager->persist($user);
+			$entityManager->flush();
+
+			$this->addFlash('success', 'Votre profil a été modifié');
+
+		}
+
+		if ($formEditLoggedUserPassword->isSubmitted() && $formEditLoggedUserPassword->isValid()) {
+
+			$user->setPassword(
+				$userPasswordHasher->hashPassword(
+					$user,
+					$formEditLoggedUserPassword->get('plainPassword')->getData()
+				)
+			);
+			$entityManager->persist($user);
+			$entityManager->flush();
+
+			$this->addFlash('success', 'Votre mot de passe a été modifié');
+
+		}
+		return $this->render('security/editLoggedUser.html.twig', [
+			'editLoggedUserForm' => $formEditLoggedUser->createView(),
+			'editLoggedUserPasswordForm' => $formEditLoggedUserPassword->createView(),
+		]);
 	}
 }
---
+++ src/Form/EditUserLoggedType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Participant;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EditUserLoggedType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+			->add('email', EmailType::class, [
+				'label' => 'Email'
+			])
+			->add('pseudo', TextType::class)
+			->add('nom', TextType::class)
+			->add('prenom', TextType::class)
+			->add('telephone', TextType::class, [
+				'required'=>false,
+				'label' => 'Téléphone'
+			])
+            ->add('isActif')
+			->add('image', TextType::class, [
+				'required'=>false,
+				'label' => 'Lien vers une photo'
+			])
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Participant::class,
+        ]);
+    }
+}
