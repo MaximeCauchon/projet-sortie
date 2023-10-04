f8419357555d84b9d11ef064ef04326c66592710
MaximeCauchon <m.cauchon1985@gmail.com>
2023-10-04 11:59:17 +0200

    modifier et afficher sortie : correction route

--- src/Form/ModifSortieType.php
+++ src/Form/ModifSortieType.php
@@ -2,18 +2,15 @@
 
 namespace App\Form;
 
-
 use App\Entity\Lieu;
 use App\Entity\Ville;
-use App\Entity\Sortie;;
-
+use App\Entity\Sortie;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\DateIntervalType;
@@ -22,48 +19,47 @@ class ModifSortieType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-
         $builder
-            ->add('nom', TextType::class, [
-                'label' => 'Nom de la sortie :'
-            ])
-            ->add('dateHeureDebut', DateTimeType::class, [
-                'label' => 'Date et heure de la sortie :'
-            ])
-            ->add('dateLimiteInscription', DateTimeType::class, [
-                'label' => "Date limite d'inscription :"
-            ])
-            ->add('nbInscriptionMax', IntegerType::class, [
-                'label' => 'Nombre de places :'
-            ])
-            ->add('duree', DateIntervalType::class, [
-                'label' => 'Durée :',
-                'with_years'  => false,
-                'with_months' => false,
-                'with_days'   => false,
-                'with_minutes'  => true,
-                'labels' => [
-                    'minutes' => "minutes",
-                ]
-            ])
-            ->add('infosSortie', TextType::class, [
-                'label' => 'Descriptions et infos :'
-            ])
-            ->add('ville', EntityType::class, [
-                'label' => 'Ville :',
-                'class' => Ville::class,
-                'choice_label' => 'nom',
-                'mapped' => false
-            ])
-            ->add('lieu', EntityType::class, [
-                'label' => 'Lieu :',
-                'class' => Lieu::class,
-                'choice_label' => 'nom'
-            ])
-            ->add('enregistrer', SubmitType::class, ['label' => 'Enregistrer'])
-            ->add('publier', SubmitType::class, ['label' => 'Publier'])
-            ->add('supprimer', SubmitType::class, ['label' => 'Supprimer'])
-            ->getForm();
+        ->add('nom', TextType::class, [
+            'label' => 'Nom de la sortie :'
+        ])
+        ->add('dateHeureDebut', DateTimeType::class, [
+            'label' => 'Date et heure de la sortie :'
+        ])
+        ->add('dateLimiteInscription', DateTimeType::class, [
+            'label' => "Date limite d'inscription :"
+        ])
+        ->add('nbInscriptionMax', IntegerType::class, [
+            'label' => 'Nombre de places :'
+        ])
+        ->add('duree', DateIntervalType::class, [
+            'label' => 'Durée :',
+            'with_years'  => false,
+            'with_months' => false,
+            'with_days'   => false,
+            'with_minutes'  => true,
+            'labels' => [
+                'minutes' => "minutes",
+            ]
+        ])
+        ->add('infosSortie', TextType::class, [
+            'label' => 'Descriptions et infos :'
+        ])
+        ->add('ville', EntityType::class, [
+            'label' => 'Ville :',
+            'class' => Ville::class,
+            'choice_label' => 'nom',
+            'mapped' => false
+        ])
+        ->add('lieu', EntityType::class, [
+            'label' => 'Lieu :',
+            'class' => Lieu::class,
+            'choice_label' => 'nom'
+        ])
+        ->add('enregistrer', SubmitType::class, ['label' => 'Enregistrer'])
+        ->add('publier', SubmitType::class, ['label' => 'Publier'])
+        ->add('supprimer', SubmitType::class, ['label' => 'Supprimer'])
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
