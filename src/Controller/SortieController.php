089715d0e8c7c1771befc70f9b0381840152c329
noemie <noemie@velorizons.com>
2023-10-12 10:38:55 +0200

    annulation par admin + verif contraintes coté back

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -19,156 +19,190 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SortieController extends AbstractController
 {
-    #[Route('/nouvelle-sortie', name: 'nouvelle_sortie')]
-    public function ajoutSortie(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $sortie = new Sortie();
+	#[Route('/nouvelle-sortie', name: 'nouvelle_sortie')]
+	public function ajoutSortie(Request $request, EntityManagerInterface $entityManager): Response
+	{
+		$sortie = new Sortie();
 
-        $currentUser = $this->getUser();
-        $sortie->setOrganisateur($currentUser);
-        $etatRepo = $entityManager->getRepository(Etat::class);
-        $sortie->setEtat($etatRepo->find(1));
-        $campus = $this->getUser()->getCampus();
-        $sortie->setCampus($campus);
+		$currentUser = $this->getUser();
+		$sortie->setOrganisateur($currentUser);
+		$etatRepo = $entityManager->getRepository(Etat::class);
+		$sortie->setEtat($etatRepo->find(1));
+		$campus = $this->getUser()->getCampus();
+		$sortie->setCampus($campus);
 
-        $nouvelleSortieForm = $this->createForm(NouvelleSortieType::class, $sortie);
-        $nouvelleSortieForm->handleRequest($request);
+		$nouvelleSortieForm = $this->createForm(NouvelleSortieType::class, $sortie);
+		$nouvelleSortieForm->handleRequest($request);
 
-        if ($nouvelleSortieForm->isSubmitted() && $nouvelleSortieForm->isValid()) {
+		if ($nouvelleSortieForm->isSubmitted() && $nouvelleSortieForm->isValid()) {
 
-            if ($nouvelleSortieForm->get('publier')->isClicked()) {
-                $sortie->setEtat($etatRepo->find(2));
-            }
-            $entityManager->persist($sortie);
-            $entityManager->flush();
+			if ($nouvelleSortieForm->get('publier')->isClicked()) {
+				$sortie->setEtat($etatRepo->find(2));
+			}
+			$entityManager->persist($sortie);
+			$entityManager->flush();
 			$this->addFlash('success', 'La sortie a été créée.');
-            return $this->redirect($this->generateUrl('details_sortie', ['id' => $sortie->getId()]));
-        }
+			return $this->redirect($this->generateUrl('details_sortie', ['id' => $sortie->getId()]));
+		}
 
-        $lieu = new Lieu();
+		$lieu = new Lieu();
 
-        $nouveauLieuForm = $this->createForm(NouveauLieuType::class, $lieu);
-        $nouveauLieuForm->handleRequest($request);
+		$nouveauLieuForm = $this->createForm(NouveauLieuType::class, $lieu);
+		$nouveauLieuForm->handleRequest($request);
 
-        if ($nouveauLieuForm->isSubmitted() && $nouveauLieuForm->isValid()) {
-            $entityManager->persist($lieu);
-            $entityManager->flush();
+		if ($nouveauLieuForm->isSubmitted() && $nouveauLieuForm->isValid()) {
+			$entityManager->persist($lieu);
+			$entityManager->flush();
 			$this->addFlash('success', 'Le lieu a été créé.');
-        }
-            
-        return $this->render('sortie/nouvelle-sortie.html.twig', [
-            'controller_name' => 'SortieController',
-            'nouvelleSortieForm' => $nouvelleSortieForm->createView(),
-            'nouveauLieuForm' => $nouveauLieuForm->createView()
-        ]);
-    }
+		}
 
-    #[Route('/modifier-sortie/{id}', name: 'modifier_sortie')]
-    public function modifSortie(int $id, SortieRepository $sortieRepository, Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $sortie = $sortieRepository->findSortieWithLieu($id);
+		return $this->render('sortie/nouvelle-sortie.html.twig', [
+			'controller_name' => 'SortieController',
+			'nouvelleSortieForm' => $nouvelleSortieForm->createView(),
+			'nouveauLieuForm' => $nouveauLieuForm->createView()
+		]);
+	}
 
-        if (!$sortie) {
-            throw $this->createNotFoundException("Cette sortie n'existe pas.");
-        }
+	#[Route('/modifier-sortie/{id}', name: 'modifier_sortie')]
+	public function modifSortie(int $id, SortieRepository $sortieRepository, Request $request, EntityManagerInterface $entityManager): Response
+	{
+		$sortie = $sortieRepository->findSortieWithLieu($id);
 
-        $modifSortieForm = $this->createForm(ModifSortieType::class, $sortie);
-        $modifSortieForm->handleRequest($request);
+		if (!$sortie) {
+			$this->addFlash('warning', 'Cette sortie n\'existe pas.');
+			return $this->redirectToRoute('app_affichage_sorties');
+		}
 
-        if ($modifSortieForm->isSubmitted() && $modifSortieForm->isValid()) {
+		$user = $this->getUser();
+		if (!$user->participantOrganisateurSortie($sortie)) {
+			$this->addFlash('warning', 'Vous n\'êtes pas autorisé à modifier cette sortie.');
+			return $this->redirectToRoute('app_affichage_sorties');
+		}
 
-            if ($modifSortieForm->get('supprimer')->isClicked()) {
-                $sortie->supprSortie();
-            }
+		$modifSortieForm = $this->createForm(ModifSortieType::class, $sortie);
+		$modifSortieForm->handleRequest($request);
 
-            if ($modifSortieForm->get('publier')->isClicked()) {
-                $sortie->publierSortie();
-            }
-            $entityManager->flush();
+		if ($modifSortieForm->isSubmitted() && $modifSortieForm->isValid()) {
 
-            $this->addFlash('success', 'Sortie modifiée !');
-            return $this->redirect($this->generateUrl('details_sortie', ['id' => $sortie->getId()]));
-        }
+			if ($modifSortieForm->get('supprimer')->isClicked()) {
+				$sortie->supprSortie();
+			}
 
-        return $this->render('sortie/modif-sortie.html.twig', [
-            'controller_name' => 'SortieController',
-            'sortie' => $sortie,
-            'modifSortieForm' => $modifSortieForm->createView()
-        ]);
-    }
+			if ($modifSortieForm->get('publier')->isClicked()) {
+				$sortie->publierSortie();
+			}
+			$entityManager->flush();
 
-    #[Route('/supprimer-sortie/{id}', name: 'supprimer_sortie')]
-    public function supprSortie(Sortie $sortie, EntityManagerInterface $entityManager): Response
-    {
-        $entityManager->remove($sortie);
-        $entityManager->flush();
+			$this->addFlash('success', 'Sortie modifiée !');
+			return $this->redirect($this->generateUrl('details_sortie', ['id' => $sortie->getId()]));
+		}
 
-        $this->addFlash('alert', 'Sortie supprimée !');
-        return $this->redirect($this->generateUrl('app_affichage_sorties'));
-    }
+		return $this->render('sortie/modif-sortie.html.twig', [
+			'controller_name' => 'SortieController',
+			'sortie' => $sortie,
+			'modifSortieForm' => $modifSortieForm->createView()
+		]);
+	}
 
-    #[Route('/annuler-sortie/{id}', name: 'annuler_sortie')]
-    public function annulSortie(int $id, SortieRepository $sortieRepository, EntityManagerInterface $entityManager, Request $request): Response
-    {   
+	#[Route('/supprimer-sortie/{id}', name: 'supprimer_sortie')]
+	public function supprSortie(Sortie $sortie, EntityManagerInterface $entityManager): Response
+	{
+		if (!$sortie) {
+			$this->addFlash('warning', 'Cette sortie n\'existe pas.');
+			return $this->redirectToRoute('app_affichage_sorties');
+		}
 
-        $sortie = $sortieRepository->findSortieWithLieu($id);
+		$user = $this->getUser();
+		if (!$user->participantOrganisateurSortie($sortie) || !$sortie->getEtat()->getId() == 1) {
+			$this->addFlash('warning', 'Vous n\'êtes pas autorisé à supprimer cette sortie.');
+			return $this->redirectToRoute('app_affichage_sorties');
+		}
 
-        if (!$sortie) {
-            throw $this->createNotFoundException("Cette sortie n'existe pas.");
-        }
+		$entityManager->remove($sortie);
+		$entityManager->flush();
 
-        $etatRepo = $entityManager->getRepository(Etat::class);
+		$this->addFlash('alert', 'Sortie supprimée !');
+		return $this->redirect($this->generateUrl('app_affichage_sorties'));
+	}
 
-        $annulSortieForm = $this->createForm(AnnulerSortieType::class, $sortie);
-        $annulSortieForm->handleRequest($request);
+	#[Route('/annuler-sortie/{id}', name: 'annuler_sortie')]
+	public function annulSortie(int $id, SortieRepository $sortieRepository, EntityManagerInterface $entityManager, Request $request): Response
+	{
+		$sortie = $sortieRepository->findSortieWithLieu($id);
+		if (!$sortie) {
+			$this->addFlash('warning', 'Cette sortie n\'existe pas.');
+			return $this->redirectToRoute('app_affichage_sorties');
+		}
 
-        if ($annulSortieForm->isSubmitted() && $annulSortieForm->isValid()) {
+		$user = $this->getUser();
+		if (in_array($sortie->getEtat()->getId(), [2, 3])
+			&& ($user->participantOrganisateurSortie($sortie)
+				|| $this->isGranted("ROLE_ADMIN"))) {
+			$this->addFlash('warning', 'Vous n\'êtes pas autorisé à annuler cette sortie.');
+			return $this->redirectToRoute('app_affichage_sorties');
+		}
 
-            $sortie->setEtat($etatRepo->find(7));
-            $entityManager->flush();
+		$etatRepo = $entityManager->getRepository(Etat::class);
+
+		$annulSortieForm = $this->createForm(AnnulerSortieType::class, $sortie);
+		$annulSortieForm->handleRequest($request);
+
+		if ($annulSortieForm->isSubmitted() && $annulSortieForm->isValid()) {
+
+			$sortie->setEtat($etatRepo->find(7));
+			$entityManager->flush();
 
 
-            $this->addFlash('success', 'Sortie annulée !');
-            return $this->redirect($this->generateUrl('app_affichage_sorties'));
-        }
+			$this->addFlash('success', 'Sortie annulée !');
+			return $this->redirect($this->generateUrl('app_affichage_sorties'));
+		}
 
-        return $this->render('sortie/annuler-sortie.html.twig', [
-            'controller_name' => 'SortieController',
-            'sortie' => $sortie,
-            'annulSortieForm' => $annulSortieForm->createView()
-        ]);
-    }
+		return $this->render('sortie/annuler-sortie.html.twig', [
+			'controller_name' => 'SortieController',
+			'sortie' => $sortie,
+			'annulSortieForm' => $annulSortieForm->createView()
+		]);
+	}
 
-    #[Route('/publier-sortie/{id}', name: 'publier_sortie')]
-    public function publierSortie(Sortie $sortie, EntityManagerInterface $entityManager, EtatRepository $etatRepo): Response
-    {
-        $sortie->setEtat($etatRepo->find(2));
-        $entityManager->flush();
+	#[Route('/publier-sortie/{id}', name: 'publier_sortie')]
+	public function publierSortie(Sortie $sortie, EntityManagerInterface $entityManager, EtatRepository $etatRepo): Response
+	{
 
-        $this->addFlash('success', 'Sortie ouverte aux inscriptions !');
-        return $this->redirect($this->generateUrl('app_affichage_sorties'));
-    }
+		$user = $this->getUser();
+		if (in_array($sortie->getEtat()->getId(), [1])
+			&& $user->participantOrganisateurSortie($sortie)) {
+			$this->addFlash('warning', 'Vous n\'êtes pas autorisé à publier cette sortie.');
+			return $this->redirectToRoute('app_affichage_sorties');
+		}
 
-    #[Route('/details-sortie/{id}', name: 'details_sortie')]
-    public function showSortie(int $id, SortieRepository $sortieRepository): Response
-    {
-        $sortie = $sortieRepository->find($id);
-        if (!$sortie) {
-            throw $this->createNotFoundException("Cette sortie n'existe pas.");
-        }
+		$sortie->setEtat($etatRepo->find(2));
+		$entityManager->flush();
 
-        if ($sortie->getEtat()->getId()!==6) {
-            return $this->render('sortie/details-sortie.html.twig', [
-                'controller_name' => 'SortieController',
-                'sortie' => $sortie
-            ]);
-        } else {
-            $this->addFlash(
-               'alert',
-               "Cette sortie est archivée et n'est plus consultable !"
-            );
-            return $this->redirect($this->generateUrl('app_affichage_sorties'));
-        }
-        
-    }
+		$this->addFlash('success', 'Sortie ' . $sortie->getNom() . ' ouverte aux inscriptions !');
+		return $this->redirect($this->generateUrl('app_affichage_sorties'));
+	}
+
+	#[Route('/details-sortie/{id}', name: 'details_sortie')]
+	public function showSortie(int $id, SortieRepository $sortieRepository): Response
+	{
+		$sortie = $sortieRepository->find($id);
+		if (!$sortie) {
+			$this->addFlash('warning', 'Cette sortie n\'existe pas.');
+			return $this->redirectToRoute('app_affichage_sorties');
+		}
+
+		if ($sortie->getEtat()->getId() !== 6) {
+			return $this->render('sortie/details-sortie.html.twig', [
+				'controller_name' => 'SortieController',
+				'sortie' => $sortie
+			]);
+		} else {
+			$this->addFlash(
+				'alert',
+				"Cette sortie est archivée et n'est plus consultable !"
+			);
+			return $this->redirect($this->generateUrl('app_affichage_sorties'));
+		}
+
+	}
 }
